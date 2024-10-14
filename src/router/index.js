393aa0aa79c3f91b52ec9530e2f2350b86cd82d1
loicfarel <loicfarele@gmail.com>
2024-10-14 12:02:35 +0100

    fix error create indicator gouv

--- src/router/index.js
+++ src/router/index.js
@@ -45,7 +45,6 @@ import dashboard_projets_activites_globale from "../views/dashboard/projets/acti
 import dashboard_projets_taches_globale from "../views/dashboard/projets/taches-globale/index.vue";
 import dashboard_projets_sous_composantes_globale from "../views/dashboard/projets/sous-composantes-globale/index.vue";
 import projets_id_details from "../views/dashboard/projets/_id/details.vue";
-import FormFactuel from "../views/dashboard/FormFactuel.vue";
 
 const routes = [
   {
