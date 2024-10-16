658a8ec124a895c002c498ca2687a722e4278d4c
KANONTIN VENCESLAS <CELERITEHOLDING@GMAIL.COM>
2024-10-16 11:49:47 +0100

    mise en forme

--- src/services/configs/environment.js
+++ src/services/configs/environment.js
@@ -1,10 +1,10 @@
-// const API_BASE_URL = "https://mybsd.shop"
+ const API_BASE_URL = "http://mybsd.shop"
 
 //const API_BASE_URL = "http://192.168.1.14:8000";
 
 // const API_BASE_URL = "https://se-papc.sirat.bj:8001";
 
-const API_BASE_URL = "http://192.168.1.3:8000";
+//const API_BASE_URL = "http://192.168.1.3:8000";
 
 //const API_BASE_URL = "http://192.168.1.14:8000";
 // const STAGGING_URL = "https://mybsd.shop:8000/"
