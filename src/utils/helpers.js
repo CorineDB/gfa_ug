e1dbe3925dbfb9e6e8461094725b5a13dc765ad5
loicfarel <loicfarele@gmail.com>
2024-12-17 12:48:55 +0100

    feat function verifyPermission

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -38,3 +38,19 @@ export const getFieldErrors = (errors) => {
   if (!errors || !Array.isArray(errors)) return "";
   return errors.join(", ");
 };
+
+export function verifyPermission(permission) {
+  if (JSON.parse(localStorage.getItem("authenticateUser"))) {
+    var permissions = JSON.parse(localStorage.getItem("authenticateUser")).role[0].permissions;
+  }
+  var tab = permissions
+    ? permissions.filter((item) => {
+        return permission
+          .toLowerCase()
+          .split(" ")
+          .every((v) => item.slug.toLowerCase().includes(v));
+      })
+    : [];
+
+  return tab.length > 0;
+}
