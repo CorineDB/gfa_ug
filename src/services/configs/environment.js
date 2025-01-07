29fc9343cbbee1f8b891bb2a66c49f8d87f3b8d1
loicfarel <loicfarele@gmail.com>
2025-01-07 09:57:26 +0100

    env

---
+++ src/services/configs/environment.js
@@ -0,0 +1,13 @@
+const API_BASE_URL = "http://mybsd.shop";
+
+const DEPLOYMENT_URL = "https://mybsd.shop/";
+
+const PUSHER_BASE_URL = "https://mybsd.shop/";
+
+const PUSHER_APP_KEY = "44c99c1fccbbcdb8d6fd";
+
+const PUSHER_APP_CLUSTER = "eu";
+
+const FRONT_BASE_URL = window.location.origin + "/";
+
+export { API_BASE_URL, PUSHER_APP_CLUSTER, PUSHER_APP_KEY, PUSHER_BASE_URL, FRONT_BASE_URL };
