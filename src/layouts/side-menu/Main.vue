12ce6e0153eae1107c0170c69f3fb1bbcf40027d
wens321 <c2istartup@gmail.com>
2025-06-19 10:58:07 +0100

    ee

--- src/layouts/side-menu/Main.vue
+++ src/layouts/side-menu/Main.vue
@@ -8,7 +8,7 @@
       <nav v-if="!isToolsPerception" class="fixed h-screen overflow-scroll side-nav navColor scrollbar-hidden">
         <router-link :to="{ name: 'projets' }" tag="a" class="flex flex-col items-center justify-center mt-3 intro-x">
           <h1 class="font-semibold text-white">{{ nomProgramme }}</h1>
-          <p >currentUsers.role</p>
+          <p >{{currentUsers.role}}</p>
           <!-- <img alt="Programme de redevabilité" class="w-[5rem] sm:w-[7rem]" :src="usersProfileImage" /> -->
         </router-link>
         <div class="my-6 side-nav__devider"></div>
